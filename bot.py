347e5dddb8cbbd15175092a6a873181667fc70c4
arkanovo <arknov.02@mail.ru>
2021-09-26 18:33:58 +0300

    main

--- bot.py
+++ bot.py
@@ -1,23 +1,54 @@
 import math
 import random 
-import nltk
 import json
 
 from telegram import InlineQueryResultArticle
 from telegram import InputTextMessageContent
+
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler
 from telegram.ext import Filters
 from telegram.ext import InlineQueryHandler
 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+
 TOKEN = '2024105989:AAG0GstrehAZ9hAo-NJKdzbeHippLDje66s'
 updater = Updater(token=TOKEN)
 dispatcher = updater.dispatcher
 
+
 with open('/BOT_CONFIG.json', 'r') as f:
     BOT_CONFIG = json.load(f)
 
+x = []
+y = []
+
+for intent in BOT_CONFIG['intents'].keys():
+    try:
+        for example in BOT_CONFIG['intents'][intent]['examples']:
+            x.append(example)
+            y.append(intent)
+    except:
+        pass
+
+    len(x), len(y), len(set(y))
+
+
+
+vectorizer = CountVectorizer()
+x_vect = vectorizer.fit_transform(x)
+
+log_reg = LogisticRegression()
+log_reg.fit(x_vect, y)
+
+log_reg.score(x_vect, y)
+
+def GetIntentByModel(Text):
+    return log_reg.predict(vectorizer.transform([Text]))[0]
+
+GetIntentByModel('Привет')
 
 # функция обработки команды '/start'
 def start(update, context):
